bc9d5ceff602431695c6c18232e64613a644402b
otavioaugustoafm <90585162+otavioaugustoafm@users.noreply.github.com>
2024-08-22 23:09:35 -0300

    Exercicio 1

---
+++ Ex01/javaproject/src/javaproject/classe.java
@@ -0,0 +1,16 @@
+package javaproject;
+
+import java.util.*; 
+
+public class classe {
+	
+	public static void main(String[] args) {
+		int x, a, b;
+		Scanner scanner = new Scanner(System.in);
+		a = scanner.nextInt();
+		b = scanner.nextInt();
+		x = a + b;
+		System.out.print(x);
+	}
+
+}
